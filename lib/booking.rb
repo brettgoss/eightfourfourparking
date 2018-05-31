3be0518aa17fde07b3f754946199730a84a5a8bd
Brett Goss <goss.brett@gmail.com>
2018-05-31 04:06:15 -0700

    Make add booking request to session

--- lib/booking.rb
+++ lib/booking.rb
@@ -7,18 +7,26 @@ include Helpers
 
 class Booking
   def initialize
-    get_slip
-    ap post_slip_to_session(@slip)
-    create_booking_from_session
+    @slip = get_slip
+    @session_id = post_slip_to_session(@slip)
+    create_booking_from_session(@session_id)
   end
 
   def get_slip
-    res = Helpers::get('item/19?start_date=monday&end_date=monday+4days')
-    @slip = JSON.parse(res)['item']['rate']['slip']
+    res = Helpers::get('item/4?start_date=monday&end_date=monday+4days')
+    JSON.parse(res)['item']['rate']['slip']
   end
 
   def post_slip_to_session(slip)
-    return slip
+    json_body = JSON.generate({
+      "slip": slip,
+      "form": {
+        "customer_name": "Brett Goss",
+        "customer_email": "#{ENV['CUSTOMER_EMAIL']}"
+      }
+    })
+    res = Helpers::post("booking/session", json_body)
+    ap JSON.parse(res)
   end
 
   def create_booking_from_session(session)
