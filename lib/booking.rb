b1075fa3b0a5e2640e4791aa162faa60413673ad
Brett Goss <goss.brett@gmail.com>
2018-05-31 04:16:59 -0700

    Create booking from session

--- lib/booking.rb
+++ lib/booking.rb
@@ -19,19 +19,27 @@ class Booking
 
   def post_slip_to_session(slip)
     json_body = JSON.generate({
-      "slip": slip,
-      "form": {
-        "customer_name": "Brett Goss",
-        "customer_email": "#{ENV['CUSTOMER_EMAIL']}"
-      }
+      "slip": slip
     })
     res = Helpers::post("booking/session", json_body)
     ap JSON.parse(res)
+    JSON.parse(res)['booking']['session']['id']
   end
 
-  def create_booking_from_session(session)
+  def create_booking_from_session(session_id)
+    json_body = JSON.generate({
+      "session_id": session_id,
+      "form": {
+        "customer_name": "#{ENV['CUSTOMER_NAME']}",
+        "customer_email": "#{ENV['CUSTOMER_EMAIL']}"
+      }
+    })
+    puts session_id
+    res = Helpers::post("booking/create", json_body)
+    ap JSON.parse(res)
   end
 end
+
 # Called when run on the command line but not in a require
 if __FILE__ == $PROGRAM_NAME
   Booking.new
