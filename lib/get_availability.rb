4bc1c6759c3febd2fec57d98194218ac55b5e9ab
Brett Goss <goss.brett@gmail.com>
2018-05-31 03:34:13 -0700

    Initialize booking creation

--- lib/get_availability.rb
+++ lib/get_availability.rb
@@ -1,8 +1,16 @@
 require 'dotenv'
+require_relative '../helpers'
+
 Dotenv.load
+include Helpers
 
 class Booking
+  def initialize
+    puts get_items
+  end
+
   def get_items
+    return "test"
   end
 
   def get_slip
@@ -10,5 +18,5 @@ class Booking
 end
 # Called when run on the command line but not in a require
 if __FILE__ == $PROGRAM_NAME
-  Booking.new.get_items
+  Booking.new
 end
