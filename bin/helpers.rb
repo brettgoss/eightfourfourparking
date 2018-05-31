31727c2f5da31d12655c738dc351d0a60a6e5dc3
Brett Goss <goss.brett@gmail.com>
2018-05-31 02:43:51 -0700

    Stub helper functions

---
+++ bin/helpers.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+require 'dotenv'
+require 'http'
+require 'awesome_print'
+require 'base64'
+Dotenv.load
+
+module Helpers
+  def build_url
+  end
+
+  def get
+  end
+
+  def post
+  end
+end
